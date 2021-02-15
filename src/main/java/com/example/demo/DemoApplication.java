5a3aa63f9beef6524a0d72387ab40afa5bd38d9c
onjsdnjs <onjsdnjs@gmail.com>
2021-02-15 16:57:34 +0900

    mobileSender

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,8 @@ package com.example.demo;
 import com.example.demo.core.config.annotation.EnableMobileSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableMobileSender
 public class DemoApplication {
 
--- src/main/java/com/example/demo/core/config/annotation/EnableMobileSender.java
+++ src/main/java/com/example/demo/core/config/annotation/EnableMobileSender.java
@@ -4,6 +4,7 @@ import com.example.demo.core.config.build.DefaultMobileSenderBaseClass;
 import com.example.demo.core.config.init.MobileSenderRegistrar;
 import com.example.demo.core.factory.MobileSenderFactoryBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
@@ -13,6 +14,7 @@ import java.lang.annotation.*;
 @Documented
 @Inherited
 @Import(MobileSenderRegistrar.class)
+@Configuration
 public @interface EnableMobileSender {
 
     String[] value() default {};
