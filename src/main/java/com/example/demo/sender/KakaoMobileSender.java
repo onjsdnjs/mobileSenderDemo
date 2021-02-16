49279c9cbef46d9fb3bbcaa4f09e403601b8425f
onjsdnjs <onjsdnjs@gmail.com>
2021-02-16 11:20:15 +0900

    mobileSender

--- src/main/java/com/example/demo/sender/KakaoMobileSender.java
+++ src/main/java/com/example/demo/sender/KakaoMobileSender.java
@@ -1,9 +1,7 @@
 package com.example.demo.sender;
 
-import com.example.demo.core.config.annotation.NoMobileSenderBean;
 import com.example.demo.core.sender.MobileSender;
 import com.example.demo.dto.KakaoMobileRequest;
 import com.example.demo.dto.KakaoMobileResponse;
 
-//@NoMobileSenderBean
 public interface KakaoMobileSender extends MobileSender<KakaoMobileRequest, KakaoMobileResponse> { }
