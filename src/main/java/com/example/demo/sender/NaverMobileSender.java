e6f55656b50a5ea5d8f09f98bd76baca4f84a9fc
onjsdnjs <onjsdnjs@gmail.com>
2021-02-17 09:38:26 +0900

    mobile Sender

--- src/main/java/com/example/demo/sender/NaverMobileSender.java
+++ src/main/java/com/example/demo/sender/NaverMobileSender.java
@@ -4,4 +4,8 @@ import com.example.demo.core.sender.MobileSender;
 import com.example.demo.dto.NaverMobileRequest;
 import com.example.demo.dto.NaverMobileResponse;
 
-public interface NaverMobileSender extends MobileSender<NaverMobileRequest, NaverMobileResponse> { }
+public interface NaverMobileSender extends MobileSender<NaverMobileRequest, NaverMobileResponse> {
+
+    String sendCount();
+
+}
